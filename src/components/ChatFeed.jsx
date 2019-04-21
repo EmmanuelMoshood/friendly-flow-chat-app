f32d994f02c03fa22d5ced641636319fd26d9e2e
EmmanuelMoshood <emmanuelmoshood571@gmail.com>
2019-04-21 11:12:35 -0400

    created message form

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -8,6 +8,17 @@ function ChatFeed(props) {
 const {chats, activeChat, userName, messages} = props;
 const chat = chats && chats[activeChat];
 
+const renderReadReceipts = (message, isMyMessage) => chat.people.map( (person, index) => person.last_read === message.id &&  (
+  <div>
+    key = {`read_${index}`}
+    className = "read-receipt"
+    style = {{
+      float: isMyMessage ? 'right' : 'left',
+      backgroundImage : person.person.avatar & `url(${person.person.avatar})`
+    }}
+  </div>
+))
+
 const renderMessages = () => {
   const keys = Object.keys(messages);
 
@@ -27,7 +38,7 @@ const renderMessages = () => {
         </div>
        
        <div className="read-receipts" style={{marginRight: isMyMessage ? '18px': '0px', 
-       marginLeft: isMyMessage ? '0px' : '68px'}}></div>
+       marginLeft: isMyMessage ? '0px' : '68px'}}>{renderReadReceipts(message, isMessage)}</div>
       </div>
     )
   } )
