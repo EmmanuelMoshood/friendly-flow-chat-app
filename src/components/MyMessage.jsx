76783735a183057b8055bcfc17adf5f486195916
EmmanuelMoshood <emmanuelmoshood571@gmail.com>
2019-04-24 12:37:12 -0400

    update message functions

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
-
-function MyMessage({message}) {
-  if(message.attachments && message.attachments.length > 0){
+const MyMessage = ({ message }) => {
+  if (message.attachments && message.attachments.length > 0) {
     return (
       <img
         src={message.attachments[0].file}
@@ -9,13 +7,14 @@ function MyMessage({message}) {
         className="message-image"
         style={{ float: 'right' }}
       />
-    )
+    );
   }
+
   return (
     <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
       {message.text}
     </div>
-  )
-}
+  );
+};
 
-export default MyMessage
+export default MyMessage;
